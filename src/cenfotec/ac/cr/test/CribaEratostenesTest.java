084ab4bdcc81add4a88779b01f90e848be5b7c42
enzocr <equartinoz@ucenfotec.ac.cr>
2018-07-25 22:01:52 -0600

    Se importa la clase CribaEratosteles al test

--- src/cenfotec/ac/cr/test/CribaEratostenesTest.java
+++ src/cenfotec/ac/cr/test/CribaEratostenesTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import cenfotec.ac.cr.logica.CribaEratostenes;
+
 public class CribaEratostenesTest {
 
 	static CribaEratostenes criba;
